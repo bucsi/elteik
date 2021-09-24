6d5b20454beb13b418fe0d1a0f52f6cc357221f7
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2021-09-24 13:46:23 +0200

    gyak3

--- 2021-22-1/webprog/03/lista.js
+++ 2021-22-1/webprog/03/lista.js
@@ -12,6 +12,32 @@ jeloltek.forEach(j => {
     const li = document.createElement("li");
     li.innerText = j;
     li.classList.add("jelolt");
-    li.classList.add("bejutott");
     jeloltLista.appendChild(li);
 })
+
+/**
+ *
+ * @param {Node} szulo egy HTML elem querySelectorral kiválasztva (pl. `document.querySelector("ul")`)
+ * @param {keyof HTMLElementTagNameMap} gyerek egy CSS szelektor, ami leírja azon elemeket, melyeken szeretnénk futtatni a fgv-t (pl. `"a"`)
+ * @param {string} mikor egy esemény, stringként (pl. `"click"`)
+ * @param {(ev: Event, target: Node) => null} mit A függvény, amit futtatunk
+ */
+ function delegal(szulo, gyerek, mikor, mit) {
+    function esemenyKezelo(ev) {
+        const esemenyCelja = ev.target
+        const esemenyKezeloje = this
+        const legkozelebbiKeresettElem = esemenyCelja.closest(gyerek)
+
+        if (esemenyKezeloje.contains(legkozelebbiKeresettElem)) {
+            mit(ev, legkozelebbiKeresettElem)
+        }
+    }
+
+    szulo.addEventListener(mikor, esemenyKezelo)
+}
+
+delegal(jeloltLista, ".jelolt", "click", handleClick)
+
+function handleClick(esemeny, celpont){
+    celpont.classList.toggle("bejutott")
+}
