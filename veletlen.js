763dd7891ea25cd7f0234234f23a863ec9f99708
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2021-10-11 17:40:32 +0200

    modultalanítás

--- veletlen.js
+++ veletlen.js
@@ -3,6 +3,6 @@
  * @param {Number} alja Egész szám, az intervallum alja
  * @param {*} teteje Egész szám, az intervallum teteje
  */
-export default function veletlenKozott(alja, teteje) {
+function veletlenKozott(alja, teteje) {
     Math.floor(Math.random() * (teteje - alja + 1) + alja)
 }
