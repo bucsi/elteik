d4525c3c415d10dc07da52f7752a0dcdd23441ae
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2021-10-18 16:25:44 +0200

    eltévedt fájlok törlése

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log(veletlenKozott(1, 3))
--- veletlen.js
+++
@@ -1,8 +0,0 @@
-/**
- * Véletlen egész számot ad vissza az [alja-teteje] zárt intervallumból.
- * @param {Number} alja Egész szám, az intervallum alja
- * @param {*} teteje Egész szám, az intervallum teteje
- */
-function veletlenKozott(alja, teteje) {
-    return Math.floor(Math.random() * (teteje - alja + 1) + alja)
-}
