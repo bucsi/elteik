2167aa79e8be837470bd2c1253525160ac65268c
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-02-11 14:48:07 +0100

    imperative first practice, webprogf 3/1

---
+++ 2021-22-2/improg/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello world\n");
+    printf("My name is Bucsi\n");
+    printf("The sum of %d + %d is: %d\n", 15, 13, 15+13);
+    float result = 13.0/3.0;
+    float myFloat = 45.789;
+    printf("Supposedly the floating point number is %f\n", result);
+    //printf("The result printed with different formatting characters is\n");
+    
+    return 0;
+}
