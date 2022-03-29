2efa7873a8314b4c3f63559efe7039f2307ff507
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-03-29 14:57:07 +0200

    zh megoldások szinkronizálása

--- 2021-22-2/webprogf-2/07-zh-gyakorlas/3-timeline/index.js
+++ 2021-22-2/webprogf-2/07-zh-gyakorlas/3-timeline/index.js
@@ -4,7 +4,68 @@ const rightButton = document.querySelector('#right');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-let pixelsPerYear = 5;
+const pixelsPerYear = 5;
 let origoYear = 1000;
 let dt = 0.2;
 
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 1000; i <= 1500; i+=50) {
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = '3px';
+    ctx.beginPath();
+    ctx.moveTo((i - origoYear) * pixelsPerYear, 0);
+    ctx.lineTo((i - origoYear) * pixelsPerYear, canvas.height);
+    ctx.stroke();
+    ctx.fillText(i, (i - origoYear) * pixelsPerYear, 10)
+  }
+
+  arpads.forEach(k => {
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeRect((k.from - origoYear) * pixelsPerYear, 100, (k.to - k.from) * pixelsPerYear, 50);
+    ctx.fillRect((k.from - origoYear) * pixelsPerYear, 100, (k.to - k.from) * pixelsPerYear, 50);
+
+    ctx.fillStyle = 'black';
+    ctx.fillText(k.name, (k.from - origoYear) * pixelsPerYear, 110);
+    ctx.fillText(`${k.from}-${k.to}`, (k.from - origoYear) * pixelsPerYear, 130);
+  });
+  
+  plantanegets.forEach(k => {
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'pink';
+    ctx.strokeRect((k.from - origoYear) * pixelsPerYear, 200, (k.to - k.from) * pixelsPerYear, 50);
+    ctx.fillRect((k.from - origoYear) * pixelsPerYear, 200, (k.to - k.from) * pixelsPerYear, 50);
+
+    ctx.fillStyle = 'black';
+    ctx.fillText(k.name, (k.from - origoYear) * pixelsPerYear, 210);
+    ctx.fillText(`${k.from}-${k.to}`, (k.from - origoYear) * pixelsPerYear, 230);
+  });
+}
+draw()
+
+leftButton.addEventListener('click', function (e) {
+  origoYear -= 10;
+  draw()
+});
+rightButton.addEventListener('click', function (e) {
+  origoYear += 10;
+  draw()
+});
+animButton.addEventListener('click', function (e) {
+  origoYear = 1000;
+  dt = 0.2;
+  animationLoop();
+})
+function animationLoop() {
+  requestAnimationFrame(animationLoop)
+  if (origoYear > 1400) {
+    dt = -0.2;
+  }
+  else if (origoYear < 1000) {
+    dt = 0.2;
+  }
+  origoYear += dt;
+  draw();
+}
--- 2021-22-2/webprogf-2/07-zh-gyakorlas/4-attendance-list/index.js
+++ 2021-22-2/webprogf-2/07-zh-gyakorlas/4-attendance-list/index.js
@@ -17,22 +17,50 @@ function delegate(parent, type, selector, handler) {
   });
 }
 
+document.querySelectorAll("input[type=checkbox]").forEach((c) => szinez(c));
+
 function szinez(checkbox) {
   if (checkbox.checked) {
     checkbox.parentElement.classList.add("present");
+  } else {
+    checkbox.parentElement.classList.remove("present");
   }
 }
 
-document.querySelectorAll("input[type=checkbox]").forEach((c) => {
-  szinez(c);
-});
-
+delegate(table, "input", "input[type=checkbox]", (ev) => szinez(ev.target));
 delegate(table, "click", "td", (ev) => {
   if (ev.target.matches("td")) {
     ev.target.children[0].focus();
   }
 });
 
-delegate(table, "input", "input[type=checkbox]", (ev) => {
-  szinez(ev.target);
+document.addEventListener("keydown", (ev) => {
+  if (benneVan(ev.key, ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"])) {
+    const focus = document.activeElement
+    const coords = xyCoord(focus.parentElement)
+    if(focus !== document.body){
+      console.log(coords)
+    }
+    if(ev.key === "ArrowRight"){
+//checkbox ->     td   -> tr         -> velem szomszédos td -> annak a checkboxa focus()
+      focus.parentElement.parentElement.children[coords.x+1].children[0].focus()
+    }
+    if(ev.key === "ArrowLeft"){
+//checkbox ->     td   -> tr         -> velem szomszédos td -> annak a checkboxa focus()
+      focus.parentElement.parentElement.children[coords.x-1].children[0].focus()
+    }
+    if(ev.key === "ArrowUp"){
+//checkbox-> td        -> tr         -> table      -> szomszéd tr -> stb...
+      focus.parentElement.parentElement.parentElement.children[coords.y-1].children[coords.x].children[0].focus()
+
+    }
+    if(ev.key === "ArrowDown"){
+//checkbox-> td        -> tr         -> table      -> szomszéd tr -> stb...
+      focus.parentElement.parentElement.parentElement.children[coords.y+1].children[coords.x].children[0].focus()
+    }
+  }
 });
+
+function benneVan(elem, tomb) {
+  return tomb.find((e) => e === elem) !== undefined;
+}
