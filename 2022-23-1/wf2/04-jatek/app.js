9a91594a0bf66f60ee09f6a50750f5eb15c85deb
bucsi <11545252+bucsi@users.noreply.github.com>
2022-10-03 20:18:10 +0200

    gyak progress

--- 2022-23-1/wf2/04-jatek/app.js
+++ 2022-23-1/wf2/04-jatek/app.js
@@ -1,1 +1,26 @@
-console.log(alma)
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+ctx.translate(canvas.width / 2, canvas.height / 2);
+const state = {
+  player: new Dino(),
+  lastFrameTime: 0,
+};
+
+function update(dt) {}
+
+function render() {
+  state.player.render()
+}
+
+function next() {
+  const currentTime = performance.now();
+  const dt = (state.lastFrameTime - currentTime) / 2000;
+
+  update(dt);
+  render();
+
+  state.lastFrameTime = currentTime;
+  requestAnimationFrame(next);
+}
+
+next();
--- 2022-23-1/wf2/04-jatek/dino.js
+++ 2022-23-1/wf2/04-jatek/dino.js
@@ -1,1 +1,13 @@
-const alma = 3;
+class Dino {
+  constructor() {
+    this.position = { x: 0, y: 0 };
+  }
+
+  render(){
+    ctx.fillRect(this.position.x, this.position.y, 50, 100)
+  }
+
+  update(){
+    return
+  }
+}
